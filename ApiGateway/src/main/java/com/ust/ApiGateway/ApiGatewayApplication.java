35b7365eca9c1073e47622f0bfc421a0b9669e07
abhi61703 <bhat61703@gmail.com>
2024-08-04 15:39:57 +0530

    Updated API Gateway

--- ApiGateway/src/main/java/com/ust/ApiGateway/ApiGatewayApplication.java
+++ ApiGateway/src/main/java/com/ust/ApiGateway/ApiGatewayApplication.java
@@ -1,9 +1,11 @@
-package com.ust.APIGateway;
+package com.ust.ApiGateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
